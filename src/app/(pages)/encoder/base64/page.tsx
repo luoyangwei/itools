36a482dc8b6d7f4ad8fa894d3d95ab0637c55e71
罗杨威 <luoyangwei@rsjia.com>
2025-06-03 19:36:55 +0800

    标题

--- src/app/(pages)/encoder/base64/page.tsx
+++ src/app/(pages)/encoder/base64/page.tsx
@@ -1,8 +1,19 @@
 export default function EncoderBase64Page() {
     return (
-        <div className="flex items-center justify-between gap-6">
-            <div className="flex-1 h-full"></div>
-            <div className="flex-1 h-full"></div>
-        </div>
+        <>
+            <aside className="w-full py-4 flex flex-col gap-2 pb-12">
+                <h1 className="text-3xl font-bold">Base64 编码/解码</h1>
+                <p className="text-base">工具可以对字符串进行Base64编码和解码</p>
+            </aside>
+
+            <div className="flex items-center justify-between gap-6">
+                <div className="flex-1 h-full bg-accent rounded-2xl min-h-56 p-4">
+                    <input placeholder="字符串" />
+                </div>
+                <div className="flex-1 h-full bg-accent rounded-2xl min-h-56 p-4">
+                    <input placeholder="结果" />
+                </div>
+            </div>
+        </>
     )
 }
